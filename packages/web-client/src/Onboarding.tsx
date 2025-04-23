3d6324ae2509c5fe225365eac3e148ade076bbef
BlazenAkali <jhoscuenca15@gmail.com>
2025-04-23 16:22:41 +0800

    added About section

--- packages/web-client/src/Onboarding.tsx
+++ packages/web-client/src/Onboarding.tsx
@@ -5,11 +5,11 @@ function Onboarding() {
   return (
     <>
     <div className="logo-container">
-        <img src='../src/assets/ChasmTitle_optimized.png' height={200}></img>
-      </div>
-      <h2 className="subtitle">Survival Platform Game</h2>
-      <h1 className="main-title">Escape the Chasm</h1>
-      <p className="tagline">Play Bold. Earn a Legacy</p><br/><br/>
+      <img src='../src/assets/ChasmTitle_optimized.png' height={200}></img>
+    </div>
+    <h2 className="subtitle">Survival Platform Game</h2>
+    <h1 className="main-title">Escape the Chasm</h1>
+    <p className="tagline">Play Bold. Earn a Legacy</p><br/><br/>
     
     <div className="video-demo">
       <iframe 
@@ -22,6 +22,14 @@ function Onboarding() {
       referrerPolicy="strict-origin-when-cross-origin" 
       allowFullScreen>
       </iframe>
+    </div><br/><br/>
+
+    <div className='about-section'>
+      <h1 className="main-title">About</h1>
+      <p className="text-content">Escape the Chasm is an arcade-survival platforming game, 
+        where the player has to rise up the depths of the earth by completing objectives,<br/> 
+        gathering resources, conquering their foes, and surviving as long as possible.</p>
+        <br/><br/>
     </div>
     </>
   );
