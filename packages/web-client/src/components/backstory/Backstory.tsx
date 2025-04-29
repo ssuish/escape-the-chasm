8c161f73f6922d1a7f3741849a56af29b52cc5c5
BlazenAkali <jhoscuenca15@gmail.com>
2025-04-29 15:12:58 +0800

    -Added more content

---
+++ packages/web-client/src/components/backstory/Backstory.tsx
@@ -0,0 +1,13 @@
+import styles from './Backstory.module.css';
+
+const Backstory = () => {
+    return (
+        <div className={styles.backstory}>
+            <div className={styles.header}>
+                <h1>Backstory</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Backstory;
--- packages/web-client/src/pages/Onboarding.tsx
+++ packages/web-client/src/pages/Onboarding.tsx
@@ -4,12 +4,16 @@ import Contact from '../components/contact/Contact';
 import Footer from '../components/footer/Footer';
 import Roadmap from '../components/roadmap/Roadmap';
 import Team from '../components/team/Team';
+import Mechanics from '../components/mechanics/Mechanics';
+import Backstory from '../components/backstory/Backstory';
 
 const Onboarding = () => {
     return(
         <>
             <Hero />
             <About />
+            <Mechanics />
+            <Backstory />
             <Roadmap />
             <Team />
             <Contact />
