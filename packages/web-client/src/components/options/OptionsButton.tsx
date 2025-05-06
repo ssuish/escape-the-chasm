4cf399c9948500c8aa48299e5d52702222f809cd
Juswa <118370974+BlazenAkali@users.noreply.github.com>
2025-05-06 13:08:16 +0800

    MainGame (#21)
    Added Main Game with:
    -map
    -options button
    
    trophy button
    Used Tailwind CSS
    
    * Main Game
    
    * Character Select

---
+++ packages/web-client/src/components/options/OptionsButton.tsx
@@ -0,0 +1,13 @@
+
+
+const OptionsButton = () => {
+    return(
+        <>
+            <div className='bg-linear-to-b from-gray-500 to-gray-700 z-1 h-15 w-15 rounded-xl border-4 border-white-100 mt-10 ml-5'>
+                <img src="../src/assets/settings.png" className="p-1.5 hover:opacity-50 hover:bg-gray-900 rounded-xl active:p-2"></img>
+            </div>
+        </>
+    )
+}
+
+export default OptionsButton;
