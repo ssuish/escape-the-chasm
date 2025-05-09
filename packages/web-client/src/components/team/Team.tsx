6805e409747e37b338b1c8c3cae71e1c35f46be4
OneEyeWasTaken <justinecruz560@gmail.com>
2025-05-09 17:36:15 +0800

    Added responsiveness to "Development Team"

--- packages/web-client/src/components/team/Team.tsx
+++ packages/web-client/src/components/team/Team.tsx
@@ -40,9 +40,10 @@ const Team = () => {
                     <h1>Development Team</h1>
                 </div>
 
-                <div className="grid grid-cols-3 pt-5 h-full w-full items-center justify-items-center ">
+                <div className="grid grid-cols-2 xl:grid-cols-3 lg:grid-cols-2  gap-x-4 gap-y-4 lg:gap-x-2 pt-5 w-full items-center justify-items-center">
+
                     {devTeam.map((member, index) => (
-                        <div key={index} className="flex flex-col items-center justify-center w-[350px] h-[250px] bg-white rounded-lg shadow-lg p-4 text-black 
+                        <div key={index} className="flex flex-col items-center justify-center max-w-[350px] max-h-[250px] lg:h-[250px] h-auto bg-white rounded-lg shadow-lg p-4 text-black 
                             hover:scale-105 transition-transform duration-300 ease-in-out">
                             <img src={member.image} alt={member.name} className="w-[600px] h-2/3 object-cover rounded-full" />
                             <h2 className="font-['Alexandria'] font-bold text-xl font-bold mt-4">{member.name}</h2>
